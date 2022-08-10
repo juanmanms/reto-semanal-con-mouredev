2f5317f4ee51cf34ea63def3b342895cd65a9c79
Juanmanms <juanmanms@gmail.com>
2022-08-10 17:04:55 +0200

    prueba

---
+++ agosto/segundo.py
@@ -0,0 +1,12 @@
+numeros = [1, 2, 5, 4, 17, 17, 4, 15, 9, 10]
+n1=0
+n2=0
+for numero in numeros:
+    if numero > n1:
+        n1 = numero
+
+    if numero > n2 and numero < n1:
+        n2 = numero
+        continue
+    continue
+print ("El segundo numero más alto es: " + str(n2))
